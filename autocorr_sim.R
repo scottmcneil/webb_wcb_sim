4a113303f16dfbdeaf78468bf8d4883b78e6704a
Scott McNeil <scott@scottmcneil.ca>
2016-07-15 14:41:15 -0400

    Updated with new DB functionality

--- autocorr_sim.R
+++ autocorr_sim.R
@@ -1,54 +1,68 @@
 library(wildclusterbootsim)
+library(DBI)
+library(dplyr)
+library(foreign)
 
-#Specify your database name
-db <- 'wildclusterbootsim.db'
+###### Specify Parameters ######
 
-#Specify database table names
-parameters_table <- 'parameters'
-p_table <- 'p_values'
+#Specify DGP function
+dgp <- autocorr_dgp
 
-#Specify all your parameters
-H0 <- 1
-boot_dist <- 'six_pt'
-boot_reps <- 399
-t <- 5
+#Specify dgp parameters
+t <- c(5, 6)
 G <- 5
 ng <- 30
-reps <- 100
 rho <- 1
+lambda <- 1
+gamma <- 1
 
-#Create parameters table (only need to run once)
-parameter_list <- list(boot_dist = boot_dist, boot_reps = boot_reps, t = t, G = G, ng = ng, rho = rho)
-db_write_param(db = db, table = parameters_table, parameters = parameter_list)
+#Create list of dgp_args
+dgp_args <- list(t = t, G = G, ng = ng, rho = rho, lambda = lambda, gamma = gamma)
 
-#Get values from parameters table
-con <- con <- dbConnect(RSQLite::SQLite(), db)
-parameters <- dbReadTable(conn = con, name = parameters_table)
-dbDisconnect(conn = con)
+#Specify bootstrap parameters
+formula <- 'Y ~ X'
+reps <- 100
+x_interest <- 'X'
+clusterby <- c('G', 'tG')
+boot_dist <- 'six_pt'
+boot_reps <- 399
+bootby <- c('G', 'tG', 'i')
+H0 <- 1
+cores <- 3
+progbar = TRUE
+save_type = 'db'
+save_file <- 'wildclusterbootsim.db'
 
-#Run your simulation on each row of parameters table
-apply(X = parameters,
-      MARGIN = 1,
-      FUN = db_write_mc,
-      db = db,
-      table = p_table,
-      reps = reps,
-      dgp = dgp,
-      formula = formula,
-      bootby = bootby,
-      clusterby = clusterby,
-      x_interest = x_interest,
-      H0 = H0,
-      cores = cores)
+###### Run Simulation ######
 
-#Get all the p-values and check rejection frequencies
-con <- con <- dbConnect(RSQLite::SQLite(), db)
+muli_wild_cluster_boot_mc(dgp = dgp, dgp_args = dgp_args, formula = formula, reps = reps,
+                          x_interest = x_interest, clusterby = clusterby, boot_dist = boot_dist, boot_reps = boot_reps,
+                          bootby = bootby,  H0 = H0, cores = cores, progbar = progbar,
+                          save_type = save_type, save_file = save_file)
+
+###### Check results
+
+#Get data
+con <- dbConnect(RSQLite::SQLite(), 'firstwildclusterbootsim.db')
 p_values <- dbReadTable(conn = con, name = 'p_values')
-colMeans(p_values < 0.05)
 dbDisconnect(conn = con)
 
-#REMOVE TABLES - ONLY USE IF YOU'RE STARTING OVER
-con <- con <- dbConnect(RSQLite::SQLite(), db)
-dbRemoveTable(conn = con, name = parameters_table)
-dbRemoveTable(conn = con, name = 'p_values')
-dbDisconnect(conn = con)
+#Check counts
+p_values %>%
+  group_by_(.dots = names(dgp_args)) %>%
+  summarise(n = n())
+
+#Get boot combination names
+boot_names <- names(p_values)[!names(p_values) %in% names(dgp_args)]
+
+#Function for getting values less than alpha
+less_than_alpha <- function(x) x < 0.05
+
+#Get rejection frequencies
+p_values %>%
+  mutate_each_(funs = funs(less_than_alpha), vars = boot_names) %>%
+  group_by_(.dots = names(dgp_args)) %>%
+  summarise_each(funs(mean))
+
+#Write results to Stata file
+write.dta(p_values, "~/p_values.dta")
